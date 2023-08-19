3259bf13c5c6710312a1c2c680c0ccf17e66f348
Sritam Behera <bsritam227@gmail.com>
2023-08-20 00:31:26 +0530

    new Features Added

--- newjoke.js
+++ newjoke.js
@@ -1,10 +1,11 @@
 
-const jokes=[`"My mom died when we couldn’t remember her blood type. As she died, she kept telling us to “be positive,” but it’s hard `,
+const Funny=[`"My mom died when we couldn’t remember her blood type. As she died, she kept telling us to “be positive,” but it’s hard `,
             `" Why is it that if you donate a kidney, people love you. But if you donate five kidneys, they call the police."`,
-            `"Dark humor is like food. Not everyone gets it." `,`"You don’t need a parachute to go skydiving. You need a parachute to go skydiving twice."`,
-            `"My grief counselor died. He was so good, I don’t even care".`,`“I’m sorry” and “I apologize” mean the same thing. Except at a funeral.`,
-            `Son: “Dad, did you get the results of the DNA test back?” Dad: “Call me George.”`,`"What is the worst combination of illnesses? Alzheimer’s and diarrhea. You’re running but can’t remember where."`,
-            `"If you were born in September, it's pretty safe to assume that your parents started their new year with a bang."`,
+            `"Dark humor is like food. Not everyone gets it." `,
+            `"You don’t need a parachute to go skydiving. You need a parachute to go skydiving twice."`,
+            `"My grief counselor died. He was so good, I don’t even care".`,
+            `“I'm sorry” and “I apologize” mean the same thing. Except at a funeral.`,
+            `"What is the worst combination of illnesses? Alzheimer’s and diarrhea. You’re running but can’t remember where."`,
             `"I was drinking a martini and the waitress screamed “does anyone know CPR?” I yelled, “I know the entire alphabet” and we all laughed and laughed. Well, except for one person."`,
             `"I visited my new friend in his apartment. He told me to make myself at home. So I threw him out. I hate having visitors."`,
             `"As I get older, I remember all the people I lost along the way. Maybe my budding career as a tour guide was not the right choice."`,
@@ -14,47 +15,177 @@ const jokes=[`"My mom died when we couldn’t remember her blood type. As she di
             `"I just read that someone in New York gets stabbed every 52 seconds. Poor guy."`,
             `"The doctor gave me one year to live, so I shot him with my gun. The judge gave me 15 years. Problem solved."`,
             `"My senior relatives liked to tease me at weddings, saying things like, “You’ll be next!” They stopped once I started doing the same to them at funerals."`,
-            `"My husband and I have reached the difficult decision that we do not want children. If anybody does, please just send me your contact details and we can drop them off tomorrow."`,
             `"My girlfriend dumped me, so I stole her wheelchair. Guess who came crawling back?"`,
-
+            
             `My friend thinks he is smart. He told me an onion is the only food that makes you cry, so I threw a coconut at his face.`,
-            `Q: Is Google male or female? A: Female, because it doesn't let you finish a sentence before making a suggestion.`,
             `Teacher: "Kids, what does the chicken give you?" Student: "Meat!" Teacher: "Very good! Now what does the pig give you?" Student: "Bacon!" Teacher: "Great! And what does the fat cow give you?" Student: "Homework!"`,
             `Roses are red. Your blood is too. You look like a monkey And belong in a zoo. Do not worry, I'll be there too. Not in the cage, But laughing at you.`,
             `Whenever your ex says, "You'll never find someone like me," the answer to that is, "That's the point."`,
             `Girlfriend: "Am I pretty or ugly?" Boyfriend: "You're both." Girlfriend: "What do you mean?" Boyfriend: "You're pretty ugly."`,
             `Light travels faster than sound. This is why some people appear bright until you hear them speak.`,
-            `Kid 1: "Hey, I bet you're still a virgin." Kid 2: "Yeah, I was a virgin until last night ." Kid 1: "As if." Kid 2: "Yeah, just ask your sister." Kid 1: "I don't have a sister." Kid 2: "You will in about nine months."`,
             `An old teacher asked her student, "If I say, 'I am beautiful,' which tense is that?" The student replied, "It is obviously past."`,
             `How is Christmas like your job? You do all the work and the fat guy in the suit gets all the credit.`,
-            `An organization is like a tree full of monkeys, all on different limbs at different levels. The monkeys on top look down and see a tree full of smiling faces. The monkeys on the bottom look up and see nothing but assholes.`,
             `A teacher asked her students to use the word "beans" in a sentence. "My father grows beans," said one girl. "My mother cooks beans," said a boy. A third student spoke up, "We are all human beans."`,
             `A boy asks his father, "Dad, are bugs good to eat?" "That's disgusting. Don't talk about things like that over dinner," the dad replies. After dinner the father asks, "Now, son, what did you want to ask me?" "Oh, nothing," the boy says. "There was a bug in your soup, but now it’s gone."`,
-            `Maria went home happy, telling her mother about how she earned $20 by climbing a tree. Her mom responded, "Maria, they just wanted to see your panties!" Maria replied, "See Mom, I was smart, I took them off!"`,
-            `A boy says to a girl, "So, sex at my place?" "Yeah!" "Okay, but I sleep in a bunk bed with my younger brother, and he thinks we're making sandwiches, so we have to have a code. Cheese means faster and tomato means harder, okay?" Later on the girl is yelling, "Cheese cheese, tomato tomato!" The younger brother says, "Stop making sandwiches! You're getting mayo all over my bed!"`,
             `Little Sally came home from school with a smile on her face, and told her mother, "Frankie Brown showed me his weenie today at the playground!" Before the mother could raise a concern, Sally went on to say, "It reminded me of a peanut." Relaxing with a hidden smile, Sally's mom asked, "Really small, was it?" Sally replied, "No, salty." Mom fainted.`,
             `Do not be racist; be like Mario. He's an Italian plumber, who was made by the Japanese, speaks English, looks like a Mexican, jumps like a black man, and grabs coins like a Jew!`,
             `Yo momma is so fat, when she sat on an iPod, she made the iPad!`,
-            `Your momma is so stupid she put airbags on her computer in case it crashed.`,
-            `Yo momma so fat when she registered for MySpace there was no space left.`,
-            `Mother: "Sweetie, make a Christmas wish." Girl: "I wish that Santa will send some clothes to those naked girls in papa's computer."`,
-            `Q: What do computers eat for a snack? A: Microchips!`,
-            `I decided to make my password "incorrect" because if I type it in wrong, my computer will remind me, "Your password is incorrect."`,
-            `Q: How can you tell if a blonde used a computer? A: There's Wite-Out all over the screen.`,
-            `What does an air conditioner have in common with a computer? They both lose efficiency as soon as you open windows.`,
-            `Q: What did the spider do on the computer?A: Made a website!`,
-            `Me: Siri, where is the best place to hide a body? Siri: The second page of a Google search.`,
-            `Q: Why shouldn't Facebook have paid $1 billion dollars for Instagram? A: They could've downloaded it for free!`,
-            `Q: What did the computer do at lunchtime? A: Had a byte!`,
             `A science teacher tells his class, "Oxygen is a must for breathing and life. It was discovered in 1773." A blonde student responds, "Thank God I was born after 1773! Otherwise I would have died without it."`,
-            `Scientists have proven that there are two things in the air that have been known to cause women to get pregnant: their legs.`,
             `Q: Why can't you trust an atom? A: Because they make up everything.`,
             `Q: Why couldn't the leopard play hide and seek? A: Because he was always spotted.`
-
-
+            
+            
         ];
+        
+        const adult=[
+            `Mother: "Sweetie, make a Christmas wish." Girl: "I wish that Santa will send some clothes to those naked girls in papa's computer."`,
+            `Scientists have proven that there are two things in the air that have been known to cause women to get pregnant: their legs.`,
+            `Son: “Dad, did you get the results of the DNA test back?” Dad: “Call me George.”`,
+            `An organization is like a tree full of monkeys, all on different limbs at different levels. The monkeys on top look down and see a tree full of smiling faces. The monkeys on the bottom look up and see nothing but assholes.`,
+            `"If you were born in September, it's pretty safe to assume that your parents started their new year with a bang."`,
+            `"My husband and I have reached the difficult decision that we do not want children. If anybody does, please just send me your contact details and we can drop them off tomorrow."`,
+            `Kid 1: "Hey, I bet you're still a virgin." Kid 2: "Yeah, I was a virgin until last night ." Kid 1: "As if." Kid 2: "Yeah, just ask your sister." Kid 1: "I don't have a sister." Kid 2: "You will in about nine months."`,
+            `A boy says to a girl, "So, sex at my place?" "Yeah!" "Okay, but I sleep in a bunk bed with my younger brother, and he thinks we're making sandwiches, so we have to have a code. Cheese means faster and tomato means harder, okay?" Later on the girl is yelling, "Cheese cheese, tomato tomato!" The younger brother says, "Stop making sandwiches! You're getting mayo all over my bed!"`,
+            `Maria went home happy, telling her mother about how she earned $20 by climbing a tree. Her mom responded, "Maria, they just wanted to see your panties!" Maria replied, "See Mom, I was smart, I took them off!"`,
+            `Give it to me! Give it to me!" she yelled. "I'm so wet, give it to me now!" She could scream all she wanted, but I was keeping the umbrella. `,
+            `They say that during sex you burn off as many calories as running eight miles. Who the hell runs eight miles in 30 seconds? `,
+            `What is the first thing a man puts in a woman when they get married? The wedding ring. `,
+            `What do you call an expert fisherman? A master baiter. `,
+            `Why did the squirrel swim on its back? To keep its nuts dry.`
+            
+        ]
+        
+        const Coding=[
+            `Q: Is Google male or female? A: Female, because it doesn't let you finish a sentence before making a suggestion.`,
+            `I decided to make my password "incorrect" because if I type it in wrong, my computer will remind me, "Your password is incorrect."`,
+            `Q: Why shouldn't Facebook have paid $1 billion dollars for Instagram? A: They could've downloaded it for free!`,
+            `What does an air conditioner have in common with a computer? They both lose efficiency as soon as you open windows.`,
+            `Q: What did the spider do on the computer?A: Made a website!`,
+            `Q: What did the computer do at lunchtime? A: Had a byte!`,
+            `Q: How can you tell if a blonde used a computer? A: There's Wite-Out all over the screen.`,
+            `Yo momma so fat when she registered for MySpace there was no space left.`,
+            `Q: What do computers eat for a snack? A: Microchips!`,
+            `Your momma is so stupid she put airbags on her computer in case it crashed.`,
+            `Me: Siri, where is the best place to hide a body? Siri: The second page of a Google search.`,
+            `Why did the two Java methods get a divorce?
+            Because they had constant arguments.`,
+            `Why did the edge server go bankrupt?
+            Because it ran out of cache.`,
+            `How many developers does it take to screw in a lightbulb?
+            None. It’s a hardware problem.`,
+            `Why did the private classes break up?
+            Because they never saw each other.`,
+            `Why did the Java developer teach his young kids about single quotes?
+            Because they build character.`,
+            `Why do programmers confuse Christmas with Halloween?
+            Because OCT 31 is the same as DEC 25.`,
+            `Why do most Java programmers wear glasses?
+            Because they don’t see sharp.`
+        ]
 
-let chance=Math.floor(Math.random(0)*jokes.length);
+const quotes=[
+    `"If you want to live a happy life, tie it to a goal, not to people or things."`,
+    ` "Never let the fear of striking out keep you from playing the game."`,
+    `"Money and success don’t change people; they merely amplify what is already there." — Will Smith`,
+    ` "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking." – Steve Jobs`,
+    ` "The purpose of our lives is to be happy." — Dalai Lama`,
+    `"Life is what happens when you're busy making other plans." — John Lennon`,
+    `"Not how long, but how well you have lived is the main thing.” — Seneca`,
+    ` “The whole secret of a successful life is to find out what is one’s destiny to do, and then do it.”– Henry Ford`,
+    ` “In order to write about life first you must live it.”– Ernest Hemingway`,
+    `5. “Sing like no one’s listening, love like you’ve never been hurt, dance like nobody’s watching, and live like it’s heaven on earth.”`,
+    ` “Curiosity about life in all of its aspects, I think, is still the secret of great creative people.” – Leo Burnett`,
+    `"The unexamined life is not worth living." — Socrates`,
+    `"Life is not a problem to be solved, but a reality to be experienced.”– Soren Kierkegaard`,
+    ` "Don’t settle for what life gives you; make life better and build something." — Ashton Kutcher`,
+    `"I like criticism. It makes you strong." — LeBron James`,
+    ` "Life is like riding a bicycle. To keep your balance, you must keep moving." — Albert Einstein`,
+    `“When you have an ass for a friend, expect nothing but kicks.”`,
+    `“Don’t poison someone whom you can kill with sweets.”`,
+    `“I have lanced many boils, but none pained like my own.”`,
+    `“Walking slowly, even the donkey will reach Lhasa.”`,
+    `“When a camel is at the foot of a mountain only then judge his height.”`,
+    `“One man’s house burns so that another may warm himself.”`,
+    `He who does not climb, will not fall either.”`,
+    `“We can’t change the direction of the wind, but we can adjust the sails.”    `,
+    `The three great mysteries: air to a bird, water to a fish, humankind to himself.”`,
+    `“They who give have all things; they who withhold have nothing.”`,
+    `“Yesterday is but a dream, tomorrow but a vision.”`,
+    `“Don’t bargain for fish which are still in the water.”`
+]
+
+let flag=1;
+let Nflag;
+
+
+document.getElementById("A").addEventListener("click",setflag1);
+document.getElementById("B").addEventListener("click",setflag2);
+document.getElementById("C").addEventListener("click",setflag3);
+document.getElementById("D").addEventListener("click",setflag4);
+
+function setflag1(){
+    flag=1;
+    setflag();
+}
+function setflag2(){
+    flag=2;
+    setflag();
+}
+function setflag3(){
+    flag=3;
+    setflag();
+}
+function setflag4(){
+    flag=4;
+    setflag();
+}
+
+
+function setflag(){
+    let stringflag=JSON.stringify(flag);
+    localStorage.setItem("flag",stringflag);
+    console.log("setflag called");
+}
+function getflag(){
+    flag=localStorage.getItem("flag");
+    flag=JSON.parse(flag);
+}
+
+getflag();
+if(flag==null){
+    flag=1;
+    setflag();
+}
+else{
+}
+
 let jokeBox=document.getElementById("container");
-    jokeBox.innerHTML=jokes[chance];
-    console.log(jokes[chance]);
+let chance;
+if(flag==1){
+    chance=Math.floor(Math.random(0)*Funny.length);
+    jokeBox.innerHTML=Funny[chance];
+    console.log(Funny[chance]);
+    console.log(flag);
+}
+else if(flag==2){
+    chance=Math.floor(Math.random(0)*quotes.length);
+    jokeBox.innerHTML=quotes[chance];
+    console.log(quotes[chance]);
+    console.log(flag);
+}
+else if(flag==3){
+    chance=Math.floor(Math.random(0)*Coding.length);
+    jokeBox.innerHTML=Coding[chance];
+    console.log(Coding[chance]);
+    console.log(flag);
+}
+else if(flag==4){
+    chance=Math.floor(Math.random(0)*adult.length);
+    jokeBox.innerHTML=adult[chance];
+    console.log(adult[chance]);
+    console.log(flag);
+}
+else{
+    console.log("Error");
+}
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-console.log(arr[chance]);
+”
